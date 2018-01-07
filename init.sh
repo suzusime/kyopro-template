19365ce0bd00f60cbd80eae453fbd12834bca0cc
suzusime <tamagawajousuiro+git@gmail.com>
2018-01-07 22:39:26 +0900

    とりあえずのテンプレ

---
+++ init.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+cp -n ./template.cpp ./a.cpp
+cp -n ./template.cpp ./b.cpp
+cp -n ./template.cpp ./c.cpp
+cp -n ./template.cpp ./d.cpp
+cp -n ./template.cpp ./e.cpp
+cp -n ./template.cpp ./f.cpp
+#cp -n ./template.cpp ./g.cpp
+#cp -n ./template.cpp ./h.cpp
+#cp -n ./template.cpp ./i.cpp
+#cp -n ./template.cpp ./j.cpp
+#cp -n ./template.cpp ./k.cpp
+#cp -n ./template.cpp ./l.cpp
---
+++ template.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define REP(i, n) for(int i = 0;i < n;i++)
+#define debug(x) cout<<#x<<": "<<x<<endl
+#define DEBUG(x) cout<<#x<<": "<<x<<endl
+int powint(int a, int x){
+	int res = 1;
+	REP(i,x){
+		res = res *a; 
+	}
+	return res;
+}
+
+using namespace std;
+
+int main(){
+	int N;
+	cin>>N;
+	cout<<N<<endl;
+	return 0;
+}
