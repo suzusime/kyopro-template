0df8c716b0bd880fc8896b9924ae94123c6c05cf
suzusime <tamagawajousuiro+git@gmail.com>
2018-12-16 03:38:34 +0900

    debug()を多変数に対応させた

--- template.cpp
+++ template.cpp
@@ -10,9 +10,6 @@ using namespace std;
 #define RALL(a) (a).rbegin(), (a).rend()
 #define EXIST(s,e) ((s).find(e)!=(s).end())
 #define PB push_back
-// debug method
-#define debug(x) cout<<#x<<": "<<x<<endl
-#define DEBUG(x) cout<<#x<<": "<<x<<endl
 // typedefs
 #define ll long long
 #define vi vector<int>
@@ -31,6 +28,17 @@ using namespace std;
 #define prvec(v) REP(i,v.size()) pr(v[i])
 #define ppr(x) cout<<x
 #define lf() cout<<endl
+// debug methods
+// usage: debug(x,y);
+#define CHOOSE(a) CHOOSE2 a
+#define CHOOSE2(a0,a1,a2,a3,a4,x,...) x
+#define debug_1(x1) cout<<#x1<<": "<<x1<<endl
+#define debug_2(x1,x2) cout<<#x1<<": "<<x1<<", "#x2<<": "<<x2<<endl
+#define debug_3(x1,x2,x3) cout<<#x1<<": "<<x1<<", "#x2<<": "<<x2<<", "#x3<<": "<<x3<<endl
+#define debug_4(x1,x2,x3,x4) cout<<#x1<<": "<<x1<<", "#x2<<": "<<x2<<", "#x3<<": "<<x3<<", "#x4<<": "<<x4<<endl
+#define debug_5(x1,x2,x3,x4,x5) cout<<#x1<<": "<<x1<<", "#x2<<": "<<x2<<", "#x3<<": "<<x3<<", "#x4<<": "<<x4<<", "#x5<<": "<<x5<<endl
+#define debug(...) CHOOSE((__VA_ARGS__,debug_5,debug_4,debug_3,debug_2,debug_1,~))(__VA_ARGS__)
+
 // const number
 const double PI  = acos(-1.0);
 const double EPS = 1e-10;
@@ -45,7 +53,7 @@ int powint(int a, int x){
 
 
 int main(){
-	cci(N);
+	cci(N); ccs(s);
 	pr(N);
 	return 0;
 }
